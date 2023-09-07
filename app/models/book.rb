61d9a5759720d89871b89ced859ef0fb9adc58a7
horunfujimoto <139210256+horunfujimoto@users.noreply.github.com>
2023-09-07 09:50:38 +0000

    [Add]応用課題7b:投稿数の前日比/前週比の表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -27,5 +27,10 @@ class Book < ApplicationRecord
     end
   end
 
+#投稿数の前日比・前週比を表示（モデル側であらかじめ特定の条件式に対して名前をつけて定義し、その名前でメソッドの様に条件式を呼び出す）
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } #Time.zone.now.all_day = 1日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } #昨日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } #先週
 
 end
